8fd20e643dc2cec45bad62739a7875837933a7db
Saurav Gharti <sp.gharti@gmail.com>
2024-01-05 17:23:16 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "muddy-bar-2",
+  url: "https://muddy-bar-2.botics.co",
+  api: "https://muddy-bar-2.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
